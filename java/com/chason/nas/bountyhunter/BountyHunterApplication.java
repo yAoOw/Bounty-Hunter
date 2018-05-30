969a76958d028eb8bcee978fec8ca294909d2b52
zhang <773935581@qq.com>
2018-05-30 09:42:19 +0800

    初始化

---
+++ java/com/chason/nas/bountyhunter/BountyHunterApplication.java
@@ -0,0 +1,12 @@
+package com.chason.nas.bountyhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BountyHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BountyHunterApplication.class, args);
+	}
+}
